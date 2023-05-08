91f3ed4771df94e2f2d62cabb929386579549b80
Yong Wang <wangyong@ymatrix.cn>
2023-05-08 08:05:15 +0800

    test: add exercise

---
+++ test/cxx/random.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+	for (int i = 0; i < 20; i++)
+		cout << random() << endl;	
+}
