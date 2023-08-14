76775ee41c9d9ced7732c08b4f859014728f039f
Yong Wang <wangyong@ymatrix.cn>
2023-08-14 03:50:35 +0000

    package: non-default utility installation

---
+++ utility/system_build.sh
@@ -0,0 +1,12 @@
+#tool setup
+sudo yum install -y pssh
+
+#tree view
+sudo yum install -y tree
+
+#expect, auto interaction
+sudo yum install -y expect
+
+#pscp
+curl -s https://raw.githubusercontent.com/yinonavraham/pscp/master/bin/setup/install.sh | bash
+
