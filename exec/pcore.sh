a32e7d7cca918217f83c041a3bc2ef7c17b48dea
Yong Wang <wangyong@ymatrix.cn>
2023-12-05 05:59:06 +0000

    pcore.sh: add dir parameter

--- exec/pcore.sh
+++ exec/pcore.sh
@@ -1,23 +1,50 @@
 #!/bin/env bash
 
+clean=false
+target=/home/wy
+#target=/home/wy/matrixdb
+corepattern="core.[0-9]*"
 if [ "x"$MASTER_DATADIR = "x" ]; then
-    target=/home/wy
-else
     target=$MASTER_DATADIR
 fi
 
-corepattern="core*[0-9]*"
-#target=/home/wy/matrixdb
+function help() {
+	echo "./pcore.sh [-d \$dir_tobe_examined] [-c]"	
+	exit
+}
+
+function parse_parameter {
+	while [ "$#" -gt 0 ]; do
+		if [ "$1" == "-d" ]; then
+			shift
+			if [ "$#" -eq 0 ]; then
+				echo "-d must followed by target dir"
+				help
+			else
+				target=$1
+				shift
+			fi
+		elif [ "$1" == "-c" ]; then
+			clean=true
+			shift
+		elif [ "$1" == "-h" ]; then
+			help
+		fi
+	done
+}
+
+print_parameter() {
+	echo "dir:"$target" cleanup:"$clean
+}
+
+parse_parameter "$@"
+print_parameter 
 
 find $target -name "$corepattern" -exec ls -lh {} +
 count=`find $target -name "$corepattern"|wc -l`
 echo "$count corefiles found in dir $target"
-if  [ $# == 1 ]; then
-	if [ "$1" = "-c" ]; then
-		find $target -name "$corepattern" -exec rm {} +
-		echo "$count corefiles removed!!!"
-	else
-		echo "unexpect parameter:" $@
-		echo "./pcore.sh [-c]" 
-	fi
+
+if  [ $clean == "true" ]; then
+	find $target -name "$corepattern" -exec rm {} +
+	echo "$count corefiles removed!!!"
 fi
