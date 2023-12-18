2018369c503a4b09e2b907c104922985f19ae075
Yong Wang <wangyong@ymatrix.cn>
2023-12-18 10:02:02 +0800

    ascii: add hexdecimal

--- tool/ascii.c
+++ tool/ascii.c
@@ -8,10 +8,18 @@ int main()
 {
     int lines = (MAX_PRINTABLE + CHAR_PER_LINE - MIN_PRINTABLE) / CHAR_PER_LINE;
     int base = MIN_PRINTABLE;
+
+	printf("Output all printable ascii, style: numeric:hexdecimal: char\n");
+
     for (int i = 0; i < lines; i++)
     {
         for (int j = 0; j < CHAR_PER_LINE && base <= MAX_PRINTABLE; j++, base++)
-            printf("%d:%c\t", base, base);
+		{
+			if (base > 99)
+				printf("%d:%x:%c\t", base, base, base);
+			else
+				printf("%d:%x:%c\t\t", base, base, base);
+		}
 
         printf("\n");
     }
