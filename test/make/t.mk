4f5bdd25e4f7a36be353ed12ba11c2adb294d729
Yong Wang <wangyong@ymatrix.cn>
2024-07-01 02:02:53 +0000

    test: makefile

--- test/make/t.mk
+++ test/make/t.mk
@@ -1,6 +1,13 @@
 # make -f t.mk -e cflags=yy -e cxxflags=zz
 install: dir= ls -l
+curdir = ../make
+absdir = $(shell readlink  -f $(curdir))
 
+phony: install print
+	
 override cflags=xx
 install:
 	@echo "hello again" ${dir} ${cflags} ${cxxflags}
+
+print:
+	@echo $(curdir) "\t" $(absdir)
