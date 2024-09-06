b2a394bc59d8c09abfc4be45df86252cb1bfa37f
Yong Wang <wangyong@ymatrix.cn>
2024-09-06 17:03:51 +0800

    cxx:: static assert of invocable

---
+++ test/cxx/invocable.cc
@@ -0,0 +1,22 @@
+//compile using --std=c++17
+#include <stdio.h>
+#include <assert.h>
+
+#include <type_traits>
+ 
+auto func2(char) -> int (*)()
+{
+    return nullptr;
+}
+ 
+int main()
+{
+    static_assert(std::is_invocable_v<int()>);
+    static_assert(not std::is_invocable_v<int(), int>);
+    static_assert(std::is_invocable_r_v<int, int()>);
+    static_assert(not std::is_invocable_r_v<int*, int()>);
+    static_assert(std::is_invocable_r_v<void, void(int), int>);
+    static_assert(not std::is_invocable_r_v<void, void(int), void>);
+    static_assert(std::is_invocable_r_v<int(*)(), decltype(func2), char>);
+    static_assert(not std::is_invocable_r_v<int(*)(), decltype(func2), void>);
+}
