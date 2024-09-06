65a38c12154b09534c8d9daf988c9ab9934899d1
Yong Wang <wangyong@ymatrix.cn>
2024-09-06 16:56:58 +0800

    cxx: assignment from map

--- test/cxx/forloop.cpp
+++ test/cxx/forloop.cpp
@@ -1,5 +1,8 @@
-#include "template.h"
+//#include "template.h"
+#include <iostream>
+#include <memory>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 //using namespace myspace;
@@ -22,12 +25,44 @@ void testforcond()
 {
 	for(int i = 1; i < 1; i++)
 	{
-		printf ("loop none\n"); 
+		cout << "loop none\n" << endl; 
 	}
+}
+
+class MyObj
+{
+	public:
+		MyObj(const string &name)
+		: name_(name)
+		{}
+
+		friend ostream& operator<<(ostream& os, const MyObj& m)
+		{
+			os << "Name" << m.name_;
+			return os;
+		}
+
+	private:
+		string name_;
+};
+ 
+void testNewFor()
+{
+	std::unordered_map<string, shared_ptr<MyObj>> umap;
+	umap["black"] = make_shared<MyObj>("cat");
+	umap["red"] = make_shared<MyObj>("tea");
+
+	for (auto &pair : umap)
+	{
+		cout << *pair.second << endl;
+	}
+
+	cout << *umap["black"] << endl;
 }
 
 int main()
 {
+	testNewFor();
 	testForLoop();
 	testforcond();
 }
