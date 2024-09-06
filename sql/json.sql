8ec40fd156286d842fbb02ac4b11094a42496396
Yong Wang <wangyong@ymatrix.cn>
2024-09-06 17:22:06 +0800

    sql: fix json.sql

--- sql/json.sql
+++ sql/json.sql
@@ -1,14 +1,16 @@
+drop table if exists api;
+
 -- scalar
 select '5'::json;
 
 -- general
-select '{"key":10, "value":"wy", "active"::true}'::json;
+select '{"key":10, "value":"wy", "active":true}'::json;
 
 -- array
 select '[1, 2, "foo", false, null]'::json;
 
 -- nested
-select '{"foo":{"a":1, "b";false}, "bar":[1, 2, false]}'::json;
+select '{"foo":{"a":1, "b":false}, "bar":[1, 2, false]}'::json;
 
 -- plain numeric
 SELECT '{"reading": 1.230e-5}'::json, '{"reading": 1.230e-5}'::jsonb;
@@ -19,7 +21,7 @@ select '"foo"'::jsonb @> '"foo"'::jsonb;
 
 -- Index
 create table api (jdoc jsonb);
-create index on api using gin(jdoc, jsonb_path_ops);
+create index on api using gin(jdoc jsonb_path_ops);
 insert into api values('{
     "guid": "9c36adc1-7fb5-4d5b-83b4-90356a46061a",
     "name": "Angela Barton",
@@ -35,16 +37,21 @@ insert into api values('{
         "qui"
     ]
 }'::jsonb);
-select * from jsonb;
+select * from api;
 
 -- found
+explain select jdoc->'guid', jdoc->'name' from api where jdoc @> '{"company": "Magnafone"}';
 select jdoc->'guid', jdoc->'name' from api where jdoc @> '{"company": "Magnafone"}';
 
 -- can't use index, ? can't be used on indexed column
+explain select jdoc->'guid', jdoc->'name' from api where jdoc -> 'tags' ? 'qui';
 select jdoc->'guid', jdoc->'name' from api where jdoc -> 'tags' ? 'qui';
 
 -- index on some path
 create index on api using gin((jdoc->'tags'));
 
+explain select jdoc->'guid', jdoc->'name' from api where jdoc @@ '$.tags[*] == "qui"';
 select jdoc->'guid', jdoc->'name' from api where jdoc @@ '$.tags[*] == "qui"';
+
+explain select jdoc->'guid', jdoc->'name' from api where jdoc @? '$.tags[*] ? (@ == "qui")';
 select jdoc->'guid', jdoc->'name' from api where jdoc @? '$.tags[*] ? (@ == "qui")';
