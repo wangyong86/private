75314e00caddaa4d2e96cdda9e55bbd7c8b1532c
Yong Wang <wangyong@ymatrix.cn>
2024-09-06 16:55:01 +0800

    cxx: right value reference and forward

--- test/cxx/reference.cpp
+++ test/cxx/reference.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
+
 class base {
 public:
 	virtual void func()
@@ -19,7 +22,7 @@ public:
 	}
 };
 
-int main(int argc, char **argv)
+void reference()
 { 
 	derived c;
 	base &e = c;
@@ -27,4 +30,59 @@ int main(int argc, char **argv)
 	derived &d = dynamic_cast<derived&>(e);
 	d.func();
 	cout << "main" << endl;
+}
+
+void move_reference()
+{
+	std::string str("hello, the world");
+
+	std::vector<std::string> vec;
+	vec.push_back(str);
+	cout << "after moved:pushback:" << str << endl; 
+
+	vec.emplace_back(std::move(str));
+	cout << "after moved:emplack:" << str << endl; 
+}
+
+template<typename T>
+void func(T& t)
+{
+	cout << "it's a lvalue" << endl;
+}
+
+template<typename T>
+void func(T&& t)
+{
+	cout << "it's a rvalue" << endl;
+}
+
+template<typename T>
+void wrap(T&& t)
+{
+	func(t);
+}
+
+template<typename T>
+void wrap1(T&& t)
+{
+	func(std::forward<T>(t));
+}
+
+void forward_reference()
+{
+	int num = 1920;
+
+	wrap(num);
+	wrap(1920);
+	wrap1(num);
+	wrap1(1920);
+}
+
+int main(int argc, char **argv)
+{
+	reference();
+	move_reference(); 
+	forward_reference(); 
+
+	return 1;
 }
