b2b5253074709bc76f9a003939923f604546cb63
Yong Wang <wangyong@ymatrix.cn>
2024-09-12 06:20:04 +0000

    cxx: constexprt

--- test/cxx/const.cc
+++ test/cxx/const.cc
@@ -2,7 +2,27 @@
 
 using namespace std;
 
-const char * a = "hello, the world";
+// const idicates a variable can't be change, a[0] = 'c' is forbidden
+const char *a = "hello, the world";
+
+// it's also a const, it's redundant generally
+constexpr int v_i1 = 100 * 10000;
+
+// it's also okay, this a const, const also can be 
+constexpr const int c_i1 = 100 * 1000;
+
+// mixed with static
+constexpr static const int sc_i1 = 100 * 1000;
+
+class constexpr_ctor {
+  public:
+	constexpr constexpr_ctor(int d_)
+		: d(d_)
+	{}
+
+  public:
+	int d; 
+};
 
 int main(int argc, char * argv[])
 {
@@ -16,6 +36,9 @@ int main(int argc, char * argv[])
     // correct, const reference a const
     const int &j = i;
 
+	// it's a variable, assignment is wrong
+	// v_i1 = 10;
+
     // error, const reference can't be assgined new value;
     // j = k;
 
@@ -27,6 +50,10 @@ int main(int argc, char * argv[])
 
     cout << i << j << k << l << endl;
 
+	constexpr constexpr_ctor t(10);
+
+	cout << "const:" << c_i1 << " constexpr var:" << v_i1 << " constexpr static const:"
+		 << sc_i1 << " constexpr obj variable:" << 10 << endl;
 #if 0
     struct const_val *p = (struct const_val*) malloc (sizeof(struct const_val)); 
    
