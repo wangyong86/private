2bfab0a2b7d973a7a3168994eb44a31e476937d0
Yong Wang <wangyong@ymatrix.cn>
2025-10-16 10:59:58 +0800

    shell: iterate file

--- test/bash/io.sh
+++ test/bash/io.sh
@@ -1,19 +1,25 @@
 #!/bin/env bash
 
-num=$[RANDOM%100+1]
+num=$((RANDOM%100+1)) # $((expr))
 while :
 do
 	read -p " we generate a number between 0 and 100, please guess it:" n
 	if [ $n -eq 0 ]; then
 		echo "value is:$num"
 	fi
- 
+
 	if [ $n -gt $num ]; then
 		echo "larger, continue"
 	elif [ $n -lt $num ]; then
 		echo "smaller, continue"
 	else
 		echo "congratulations!, value is:$num"
-		exit 0
+		break
 	fi
 done
+
+file=io.sh
+while read line
+do
+	echo $line
+done < "$file"
